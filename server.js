92c7056ebc1b38d80efd744a3c3c68d41045f973
shomka <shaman_p@mail.ru>
2021-12-18 21:48:23 +0500

    update

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) => {
 
-    const body = req.url == '/style.css'
-        ? fs.readFileSync('./style.css')
-        : fs.readFileSync('./index.html')
+    // const body = req.url == '/style.css'
+    //     ? fs.readFileSync('./style.css')
+    //     : fs.readFileSync('./index.html')
+
+    let body = null
+    try {
+        body = fs.readFileSync(`${req.url}`)
+    } catch (e) {
+        body = fs.readFileSync(`./index.html`)
+    }
     res.end(body)
 })
 const port = process.env.PORT || 3000
